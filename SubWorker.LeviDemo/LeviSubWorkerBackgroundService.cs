795dfb6cd7a0c541767a9d84ae280b43b523194a
Levi <jiunyi.c@gmail.com>
2019-10-13 20:42:20 +0800

    Add README.md and refactoring

--- SubWorker.LeviDemo/LeviSubWorkerBackgroundService.cs
+++ SubWorker.LeviDemo/LeviSubWorkerBackgroundService.cs
@@ -4,31 +4,12 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using SchedulingPractice.Core;
 
-/*
- * Mindset
- *     Architecture: Producer -> Job Queue <- ThreadPool
- *     Action Cost: query job list x 100 + acquire-failure x 10 + query job x 1
- * Producer
- *     0. Action: Fetch job then put to job queue
- *     1. Fetching job is very expensive action, need to chose right time 
- *     2. There are two fetching strategies
- *         1. If there is no job, fetch jobs after MinPrepareTime (10 seconds)
- *         2. If there is job in database, fetch jobs after NextJob.RunAt + 1 seconds
- *
- *     examples:
- *         now: 00:00:00
- *         next job: 00:00:10
- *         wait for 11 seconds
- * Consumer
- *     0. Action get job from queue then process it
- *     1. Check job status before Acquire and Process
- */
-
 namespace SubWorker.LeviDemo
 {
     public class LeviSubWorkerBackgroundService : BackgroundService
     {
-        private const string ConnectionString = "Server=.;Database=master;user id=SA;password=Levi123456";
+//        private readonly string _connectionString = "Server=.;Database=master;user id=SA;password=Levi123456";
+        private readonly string _connectionString = null;
         private readonly SimpleThreadPool _stp;
         private readonly TimeSpan _defaultFetchJobsIntervalSeconds = JobSettings.MinPrepareTime;
         private readonly TimeSpan _fetchJobsRunAtRange = JobSettings.MinPrepareTime;
@@ -44,7 +25,7 @@ namespace SubWorker.LeviDemo
         {
             await Task.Delay(1, stoppingToken);
 
-            using (var repo = new JobsRepo(ConnectionString))
+            using (var repo = new JobsRepo(_connectionString))
             {
                 while (stoppingToken.IsCancellationRequested == false)
                 {
@@ -67,8 +48,9 @@ namespace SubWorker.LeviDemo
                     {
                         if (lastJobInThisRound != null)
                         {
-                            intervalSeconds = TimeSpan.FromSeconds((lastJobInThisRound.RunAt - DateTime.Now).Seconds + 1);
-                            
+                            intervalSeconds =
+                                TimeSpan.FromSeconds((lastJobInThisRound.RunAt - DateTime.Now).Seconds + 1);
+
                             Console.WriteLine($"[Producer][WaitForNextJob] {intervalSeconds.TotalSeconds} seconds");
                         }
                         else
