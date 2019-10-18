283677db2df99c3659f4dae509ec9503ad802487
DING-NB\Ding <andy19900208@gmail.com>
2019-10-18 22:54:31 +0800

    AndyDemo

---
+++ SubWorker.AndyDemo/AndySubWorkerBackgroundService.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.Hosting;
+using SchedulingPractice.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace SubWorker.AndyDemo
+{
+    public class AndySubWorkerBackgroundService : BackgroundService
+    {
+        private BlockingCollection<JobInfo> queue = new BlockingCollection<JobInfo>();
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Delay(1);
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread T = new Thread(Worker);
+                threads.Add(T);
+                T.Start();
+            }
+
+
+            using (JobsRepo repo = new JobsRepo())
+            {
+                while (stoppingToken.IsCancellationRequested == false)
+                {
+                    DateTime LastQueryTime = DateTime.Now;
+
+                    foreach (JobInfo job in repo.GetReadyJobs(JobSettings.MinPrepareTime))
+                    {
+                        if (stoppingToken.IsCancellationRequested == true)
+                        {
+                            break;
+                        }
+
+                        if (repo.GetJob(job.Id).State == 0 && repo.AcquireJobLock(job.Id))
+                        {
+                            if (job.RunAt > DateTime.Now)
+                            {
+                                await Task.Delay(job.RunAt - DateTime.Now);
+                            }
+                            queue.Add(job);
+                        }
+
+                    }
+
+                    if (stoppingToken.IsCancellationRequested == false
+                        && LastQueryTime.Add(JobSettings.MinPrepareTime) > DateTime.Now
+                    )
+                    {
+                        await Task.Delay(LastQueryTime.Add(JobSettings.MinPrepareTime) - DateTime.Now);
+                    }
+
+                }
+
+            }
+
+
+            queue.CompleteAdding();
+
+            threads.ForEach(T =>
+            {
+                T.Join();
+            });
+
+            Console.WriteLine($"- shutdown event detected, stop worker service...");
+        }
+    
+        private void Worker()
+        {
+            using (JobsRepo repo = new JobsRepo())
+            {
+                foreach (JobInfo job in this.queue.GetConsumingEnumerable())
+                {
+                    repo.ProcessLockedJob(job.Id);
+                }
+            }
+
+
+        }
+    }
+}
+
---
+++ SubWorker.AndyDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using SchedulingPractice.Core;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace SubWorker.AndyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<AndySubWorkerBackgroundService>();
+                })
+                .Build();
+
+            using (host)
+            {
+                host.Start();
+                host.WaitForShutdown();
+            }
+        }
+    }
+    /* <<< Best Performance : 1 Process >>>
+    Jobs Scheduling Metrics:
+
+    --(action count)----------------------------------------------
+    - CREATE:             1747
+    - ACQUIRE_SUCCESS:    1747
+    - ACQUIRE_FAILURE:    0
+    - COMPLETE:           1747
+    - QUERYJOB:           1747
+    - QUERYLIST:          66
+
+    --(state count)----------------------------------------------
+    - COUNT(CREATE):      0
+    - COUNT(LOCK):        0
+    - COUNT(COMPLETE):    1747
+
+    --(statistics)----------------------------------------------
+    - DELAY(Average):     188
+    - DELAY(Stdev):       116.2361386313
+
+    --(test result)----------------------------------------------
+    - Complete Job:       True, 1747 / 1747
+    - Delay Too Long:     0
+    - Fail Job:           True, 0
+
+    --(benchmark score)----------------------------------------------
+    - Exec Cost Score:      8347 (querylist x 100 + acquire-failure x 10 + queryjob x 1)
+    - Efficient Score:      304.24 (average + stdev) 
+    */
+    /* <<< High Availability : 5 Process >>>
+    Jobs Scheduling Metrics:
+
+    --(action count)----------------------------------------------
+    - CREATE:             1746
+    - ACQUIRE_SUCCESS:    1746
+    - ACQUIRE_FAILURE:    2541
+    - COMPLETE:           1746
+    - QUERYJOB:           7637
+    - QUERYLIST:          330
+
+    --(state count)----------------------------------------------
+    - COUNT(CREATE):      0
+    - COUNT(LOCK):        0
+    - COUNT(COMPLETE):    1746
+
+    --(statistics)----------------------------------------------
+    - DELAY(Average):     129
+    - DELAY(Stdev):       62.2094090562968
+
+    --(test result)----------------------------------------------
+    - Complete Job:       True, 1746 / 1746
+    - Delay Too Long:     0
+    - Fail Job:           True, 0
+
+    --(benchmark score)----------------------------------------------
+    - Exec Cost Score:      66047 (querylist x 100 + acquire-failure x 10 + queryjob x 1)
+    - Efficient Score:      191.21 (average + stdev)
+    */
+}
